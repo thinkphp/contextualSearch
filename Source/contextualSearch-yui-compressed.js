a4c19530eb7f7033a564fb0e06042548c3c0e2ae
Adrian Statescu <mergesortv@gmail.com>
2010-05-19 02:07:52 -0700

    updated header YAML

--- Source/contextualSearch-yui-compressed.js
+++ Source/contextualSearch-yui-compressed.js
@@ -10,10 +10,10 @@ license:
 - MIT-style license
 
 requires:
-core/1.2.1: '*'
+ core/1.2.1: '*'
 
-provides:
-- instance contextualSearch
+provides: [contextualSearch]
 
+...
 */
 var contextualSearch=new Class({Implements:[Options,Events],options:{site:"davidwalsh.name",targetID:"searchBox",css:"style.css"},boxInit:'<div id="moo-search-module" class="fadeBox"><div class="hd"><p class="title">Search on {site}</p></div><div class="bd"><div id="moo-search-module-inputs"><input type="text" id="search-input" accesskey="d"><button id="search-button">Search!</button></div><div id="moo-search-module-results"></div></div></div>',initialize:function(options){this.setOptions(options);$(this.options.targetID).set("html",this.boxInit.replace("{site}",this.options.site));if(this.options.css){this.addSkin()}$("search-button").addEvent("click",function(){this.fetchData()}.bind(this));$("search-input").addEvent("keydown",function(event){if(event.key=="enter"){this.fetchData()}}.bind(this))},fetchData:function(){var input=$("search-input").get("value"),root="http://query.yahooapis.com/v1/public/yql?q=",yqlQuery='select title,url,abstract from search.web where query="'+input+'" and  sites="'+this.options.site+'"';url=root+encodeURIComponent(yqlQuery)+"&format=json";new Request.JSONP({url:url,onRequest:function(){this.fireEvent("request");var resultsOutput=$("moo-search-module-results");resultsOutput.set("html","<p><b>Loading...</b></p>")}.bind(this),onComplete:function(searchResults){var resultsOutput=$("moo-search-module-results"),liTemplate='<li><a href="{url}">{title}</a></li>',markup="",i;if(searchResults&&(searchResults.query.count==1)){markup="<ul>";markup+=liTemplate.substitute(searchResults.query.results.result);markup+="</ul>"}else{if(searchResults&&(searchResults.query.count>1)){markup="<ul>";for(i=0;i<searchResults.query.count;i++){markup+=liTemplate.substitute(searchResults.query.results.result[i])}markup+="</ul>"}else{markup="<p>No results returned.</p>"}}resultsOutput.set("html",markup);this.fireEvent("complete",[searchResults])}.bind(this)}).send();return this},addSkin:function(){var style=document.createElement("link"),head=document.getElementsByTagName("head")[0];style.setAttribute("rel","stylesheet");style.setAttribute("type","text/css");style.setAttribute("href",this.options.css);head.insertBefore(style,head.firstChild)}});
